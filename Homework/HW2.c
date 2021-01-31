e8bd4fa0bca66b3b6a646745e0f8147fad4f5717
arthur.04123 <Arthur.04123@gmail.com>
2021-01-30 20:48:30 -0600

    p

--- Homework/HW2.c
+++ Homework/HW2.c
@@ -4,8 +4,8 @@
 int main (void) {
 	
 	float circumference;
-	double radius;
-	double area;
+	float radius;
+	float area;
 	float pi = 3.14;
 		
 	
@@ -14,10 +14,10 @@ int main (void) {
 	
 
     radius = circumference / (2*(pi));
-	area = (circumference * (radius * radius));
+	area = pi * (radius * radius);
 	
-	printf("The radius is: %lf", radius);
-	printf("\nThe area is: %lf",  area);
+	printf("The radius is: %.2f", radius);
+	printf("\nThe area is: %f",  area);
 	
 	return(0);
 
