c2f02f93d0840984a93c6eb0688ac58760f3564a
arthur.04123 <Arthur.04123@gmail.com>
2021-01-30 20:28:09 -0600

    f

--- Homework/HW2.c
+++ Homework/HW2.c
@@ -2,10 +2,22 @@
 #include <math.h>
 
 int main (void) {
-	double lett;
 	
-printf("ditto");
-scanf("%lf", &lett);
+	float circumference;
+	double radius;
+	double area;
+	float pi = 3.14;
+		
+	
+	printf("What is the wheel's circumference? ");
+	scanf("%f", &circumference);
+	
+
+    radius = circumference / (2*(pi));
+	area = (circumference * (radius * radius));
+	
+	printf("The radius is: %lf", radius);
+	printf("\nThe area is: %lf",  area);
 	
 	return(0);
 
